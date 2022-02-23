939c8abe61206f94696a6768268f66e7f076f6ac
glitzerbargeld <torben.jaeckel@me.com>
2022-02-23 14:42:08 +0100

    first commit

---
+++ trustedshops-add-widgets.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Plugin Name: Trusted Shops Widgets
+ * Description: Fügt Trusted Shops Widget Scripts in den Header ein.
+ */
+
+add_action('wp_enqueue_scripts', 'add_ts_scripts');
+
+ function add_ts_scripts() {
+    wp_register_script('ts-reviews-async-defer', 'https://integrations.etrusted.com/applications/widget.js/v2', array('jquery'), '2.0', true);
+    wp_enqueue_script('ts_reviews-async-defer');
+ }
